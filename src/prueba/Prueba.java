160914f42969339467753858330596068692e87c
Fernando Enrique Avendaño Hernández <fernando.avendano@uabc.edu.mx>
2015-11-24 19:51:48 -0800

    Cambios en Prueba.java

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -15,8 +15,8 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Hello World!");
-        System.out.println("Hola Mundo!");
+        int edad = 14;
+        System.out.println((edad < 18) ? "No puedes votar" : "Puedes votar");
     }
     
 }
